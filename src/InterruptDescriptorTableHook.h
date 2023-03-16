863eb70d8ef5dc50fc6217e0505f391e2be1f57c
rwozn <127620405+rwozn@users.noreply.github.com>
2023-03-16 11:27:06 +0100

    Delete src directory

--- src/InterruptDescriptorTableHook.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void* hookInterruptServiceRoutine(unsigned long interruptIndex, void* hookFunction);
-
-void* unhookInterruptServiceRoutine(unsigned long interruptIndex, void* originalInterruptServiceRoutine);
--- src/SystemServiceDescriptorTableHook.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void hookNtFunction(void* zwFunction, void* hookFunction);
-
-void unhookNtFunction(void* zwFunction);
